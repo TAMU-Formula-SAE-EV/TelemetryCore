8af268c51b7cf5af844a11a78b0a6f6bdc23f2d9
Juicestus <jus@gtsbr.org>
2025-03-18 22:48:41 -0500

    Clean serialinterface

--- src/serial_interface.cc
+++ src/serial_interface.cc
@@ -1,8 +1,5 @@
 #include "serial_interface.h"
 
-
-
-
 #if defined(__APPLE__) // function definitions for apple
 
 bool SerialInterface::Connect(const std::string& port_name, int baudrate) 
@@ -24,36 +21,17 @@ SerialInterface::~SerialInterface(void)
    
 #elif defined(_WIN32) // function definitions for windows
 
-//old function commented out
-
-// bool SerialInterface::Connect(const std::string& port_name, int baudrate) 
-// {
-//     const char mode[] = {'8','N','1',0};
-//     const char* target = ("\\\\.\\" + port_name).c_str();
-
-//     for (int i = 0; i < sizeof(comports); i++) {
-//         if (!strcmp(comports[i], target)) {
-//             if (RS232_OpenComport(i, baudrate, mode, 0)) {
-//                 std::cout << "[SerialError] could not open serial port " << port_name << "\n";
-//                 return false;
-//             }
-//             return true;
-//         }
-//     }
-//     return false;
-//     std::cout << "[SerialError] could not find serial port " << port_name << "\n";
-// }
-
 bool SerialInterface::Connect(const std::string& port_name, int baudrate) 
 {
    const char mode[] = {'8','N','1',0}; // Serial port mode settings
 
-   // Convert the port name to the format expected by RS232_GetPortnr (so this ensures the port name is in the correct format, which is required by the RS232_GetPortnr function to identify the correct serial port.)
-
-   std::string portStr = port_name;
+   // Convert the port name to the format expected by RS232_GetPortnr 
+   // so this ensures the port name is in the correct format, which 
+   // is required by the RS232_GetPortnr function to identify 
+   // the correct serial port.
 
    // using the library function to get the port number
-   this->cport_nr = RS232_GetPortnr(portStr.c_str());
+   this->cport_nr = RS232_GetPortnr(port_name.c_str());
 
    // seeing if the port number is valid
    if (this->cport_nr == -1) {
@@ -69,14 +47,6 @@ bool SerialInterface::Connect(const std::string& port_name, int baudrate)
 
    return true; // yay I connected to the serial port
 }
-
-
-
-
-
-
-
-
 size_t SerialInterface::Read(uint8_t* buffer, size_t size)
 {
     return RS232_PollComport(cport_nr, buffer, size); 
