9140c4fd23720d25e23d2d05b60891af5d1a43ee
FSAE <beans@beans.com>
2025-04-02 03:05:52 -0500

    fuckyou

--- src/main.cc
+++ src/main.cc
@@ -20,7 +20,7 @@
 #define VERBOSE_STATE       // send info on state update
 #define VERBOSE_WS          // send info about ws send
 #define STATEFULL_WS        // collect frames in a map and send all updates at once
-#define SPOOF_SERIAL        // do not use serial 
+//#define SPOOF_SERIAL        // do not use serial 
 
 // this is the type of function sigaction uses
 // typedef for better ergonomics
@@ -40,12 +40,15 @@ struct Core {
     Core() : identifier{}, mapper{}, serial{}, socket{}, http{} {
         log = std::ofstream("db.txt", std::ios::app);
     }
-    int Run(const std::string& serial_port, const std::string& cfg_file, uint16_t http_port, uint16_t ws_port);
+    int Run(const std::string& serial_port, const std::string& cfg_file, uint16_t http_port, uint16_t ws_port, 
+		uint32_t baud, uint64_t update_epsilion);
     sig_handler_t TermHandler(void);
 };
 
 // actual main function 
-int Core::Run(const std::string& serial_port, const std::string& cfg_file, uint16_t http_port, uint16_t ws_port) {
+int Core::Run(const std::string& serial_port, const std::string& cfg_file, uint16_t http_port, uint16_t ws_port,
+		uint32_t baud, uint64_t update_epsilion)
+{
     
     if (!mapper.LoadMappings(cfg_file)) std::cerr << "[WARNING] Encountered some error while loading mappings from config file\n"; 
 
@@ -53,7 +56,7 @@ int Core::Run(const std::string& serial_port, const std::string& cfg_file, uint1
     std::cout << mapper.Str() << "\n";
 
 #ifndef SPOOF_SERIAL
-    if (!serial.Connect(serial_port, 1152000)) return -1;    // pretty fast baudrate
+    if (!serial.Connect(serial_port, baud)) return -1;    // pretty fast baudrate
                                                             // 115200 baud = 14.4 KB/s ~= 14 KB/s
                                                             // (14 KB/s) / (14 B/frame) = 1000 frames/s
 #endif
@@ -70,7 +73,7 @@ int Core::Run(const std::string& serial_port, const std::string& cfg_file, uint1
     while (true) {
         // super-fast scheduler
         uint64_t t_now = Utils::PreciseTime<int64_t, Utils::t_us>();
-        if (t_now - t_mount < 1000) continue;       // use an epsilon oops
+        if (t_now - t_mount < update_epsilion) continue;       // use an epsilon oops
         t_mount = t_now;
 
 #ifdef SPOOF_SERIAL
@@ -166,7 +169,7 @@ sig_handler_t Core::TermHandler(void)
 int main(int argc, char** argv)
 {
 #ifdef _WIN32
-    std::string serial_port = "COM1";
+    std::string serial_port = "COM3";
 #else
     std::string serial_port = "/dev/ttys1";
 #endif
@@ -188,9 +191,9 @@ int main(int argc, char** argv)
     #endif
 
     // Run telemetry with com port and config file
-    core.Run(serial_port, "test.cfg", 9000, 9001);
+    core.Run(serial_port, "test.cfg", 80, 9001, 115200, 10);
 
-    std::cout << "wtf\n";
+    return 0;
 }
 
 
