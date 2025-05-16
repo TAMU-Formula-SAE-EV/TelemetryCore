f0903339d1ef53c7ee3856a957f20082615773a8
celizondo <calebelizondo@tamu.edu>
2025-05-16 12:26:37 -0500

    fixed line endings?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+
+
 cd test
-python3 ./simulate_serial.py &
+python3 ./simulate_serial.py & 
 
 cd ..
 sleep 3
 
-while [ true ]
+while [true]
 do
     ./bin/telemetrycore -c ./test.cfg -s /dev/pts/1 -e 10000 --http-port 9000 --ws-port 9001 -a ./client-roadrunner
 done
